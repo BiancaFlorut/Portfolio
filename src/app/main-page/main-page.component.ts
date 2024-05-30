18debce7067e46441641e0ec2abd00a3ec2c51c7
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-05-30 14:45:14 +0200

    above the fold section

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../shared/header/header.component';
 import { FooterComponent } from '../shared/footer/footer.component';
+import { AboveTheFoldComponent } from './above-the-fold/above-the-fold.component';
 
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [HeaderComponent, FooterComponent],
+  imports: [AboveTheFoldComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
