517dad147f8b1a691d8b85023f51ebd95644d4f0
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-06-06 15:12:17 +0200

    translate above the fold

--- src/app/main-page/above-the-fold/above-the-fold.component.ts
+++ src/app/main-page/above-the-fold/above-the-fold.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-above-the-fold',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule, CommonModule],
   templateUrl: './above-the-fold.component.html',
   styleUrl: './above-the-fold.component.scss'
 })
 export class AboveTheFoldComponent {
 
+  constructor(private translate: TranslateService) {}
+  getTranslatedText(key: string) {
+    return this.translate.instant(key);
+  }
+  
+  getLangStyleClass() {
+    switch (this.translate.currentLang) {
+      case 'en':
+        return 'en';
+      case 'de':
+        return 'de';
+      case 'ro':
+        return 'ro';
+      default:
+        return 'en';
+    }
+  }
 }
