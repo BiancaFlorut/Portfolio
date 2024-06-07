0cbbd670867a21bf039588d9717600dcf03bd5cc
Bianca Floruț <150796482+BiancaFlorut@users.noreply.github.com>
2024-06-07 16:56:12 +0200

    my work and imprint translated

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
--- src/app/main-page/skill-set/skill-set.component.ts
+++ src/app/main-page/skill-set/skill-set.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-skill-set',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './skill-set.component.html',
   styleUrl: './skill-set.component.scss'
 })
